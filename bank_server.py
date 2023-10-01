85e95e68de0cb646c1272c89a6e60fde081ffe9c
Brant Cheikes <brant@pedal-dancer.net>
2023-10-01 10:01:46 -0400

    Bank server done for now

--- bank_server.py
+++ bank_server.py
@@ -10,10 +10,18 @@ PORT = 65432            # Port to listen on (non-privileged ports are > 1023)
 ALL_ACCOUNTS = dict()   # initialize an empty dictionary
 ACCT_FILE = "accounts.txt"
 
-# Helper functions for Bank Server
+##########################################################
+#                                                        #
+# Bank Server Core Functions                             #
+#                                                        #
+# No Changes Needed in This Section                      #
+#                                                        #
+##########################################################
+
 def acctNumberIsValid(ac_num):
-    # a valid account number must be a string, lenth = 8, and match the format AA-NNNNN where AA are two alphabetic characters
-    # and NNNNN are five numeric characters
+    """Return True if ac_num represents a valid account number. This does NOT test whether the account actually exists, only
+    whether the value of ac_num is properly formatted to be used as an account number.  A valid account number must be a string,
+    lenth = 8, and match the format AA-NNNNN where AA are two alphabetic characters and NNNNN are five numeric characters."""
     return isinstance(ac_num, str) and \
         len(ac_num) == 8 and \
         ac_num[2] == '-' and \
@@ -21,22 +29,24 @@ def acctNumberIsValid(ac_num):
         ac_num[3:8].isdigit()
 
 def acctPinIsValid(pin):
+    """Return True if pin represents a valid PIN number. A valid PIN number is a four-character string of only numeric characters."""
     return (isinstance(pin, str) and \
         len(pin) == 4 and \
         pin.isdigit())
 
 def amountIsValid(amount):
-    # For an amount to be valid it must be a positive float() value with at most two decimal places
+    """Return True if amount represents a valid amount for banking transactins. For an amount to be valid it must be a positive float()
+    value with at most two decimal places."""
     return isinstance(amount, float) and (round(amount, 2) == amount) and (amount >= 0)
 
-# Each bank account is stored as a BankAccount instance
 class BankAccount:
-    acct_number = ''
-    acct_pin = ''
-    acct_balance = 0.0
+    """BankAccount instances are used to encapsulate various details about individual bank accounts."""
+    acct_number = ''        # a unique account number
+    acct_pin = ''           # a four-digit PIN code represented as a string
+    acct_balance = 0.0      # a float value of no more than two decimal places
     
     def __init__(self, ac_num = "zz-00000", ac_pin = "0000", bal = 0.0):
-        # initialize the state variables of a new BankAccount instance
+        """ Initialize the state variables of a new BankAccount instance. """
         if acctNumberIsValid(ac_num):
             self.acct_number = ac_num
         if acctPinIsValid(ac_pin):
@@ -45,26 +55,21 @@ class BankAccount:
             self.acct_balance = bal
 
     def deposit(self, amount):
-        # if the amount is valid, then update the acct_balance
-        # return three values: self, success_code, current balance
-        # Success codes:
-        #  0: valid result
-        #  1: invalid amount
+        """ Make a deposit. The value of amount must be valid for bank transactions. If amount is valid, update the acct_balance.
+        This method returns three values: self, success_code, current balance.
+        Success codes are: 0: valid result; 1: invalid amount given. """
         success_code = 0
         if not amountIsValid(amount):
             success_code = 1
         else:
             # valid amount, so add it to balance and set succes_code 1
             self.acct_balance += amount
-        return self, success_code, self.acct_balance
+        return self, success_code, round(self.acct_balance,2)
 
     def withdraw(self, amount):
-        # if the amount is valid, then update the acct_balance
-        # return three values: self, success_code, current balance
-        # Success codes:
-        #  0: valid result
-        #  1: invalid amount
-        #  2: attempt to overdraw account
+        """ Make a withdrawal. The value of amount must be valid for bank transactions. If amount is valid, update the acct_balance.
+        This method returns three values: self, success_code, current balance.
+        Success codes are: 0: valid result; 1: invalid amount given; 2: attempted overdraft. """
         success_code = 0
         if not amountIsValid(amount):
             # invalid amount, return error 
@@ -75,31 +80,37 @@ class BankAccount:
         else:
             # all checks out, subtract amount from the balance
             self.acct_balance -= amount
-        return self, success_code, self.acct_balance
+        return self, success_code, round(self.acct_balance,2)
 
 def get_acct(acct_num):
-    # Lookup acct_num in the ALL_ACCOUNTS database and return the account object if it's found.
-    # Return False if the acct_num is invalid. If the 
+    """ Lookup acct_num in the ALL_ACCOUNTS database and return the account object if it's found.
+        Return False if the acct_num is invalid. """
     if acctNumberIsValid(acct_num) and (acct_num in ALL_ACCOUNTS):
         return ALL_ACCOUNTS[acct_num]
     else:
         return False
 
 def load_account(num_str, pin_str, bal_str):
-    # all arguments are strings
+    """ Load a presumably new account into the in-memory database. All supplied arguments are expected to be strings. """
     try:
+        # it is possible that bal_str does not represent a float, so be sure to catch that error.
         bal = float(bal_str)
-        if acctNumberIsValid(num_str) and not get_acct(num_str):
-        # We have a valid new account number not previously loaded
+        if acctNumberIsValid(num_str):
+            if get_acct(num_str):
+                print(f"Duplicate account detected: {num_str} - ignored")
+                return False
+            # We have a valid new account number not previously loaded
             new_acct = BankAccount(num_str, pin_str, bal)
+            # Add the new account instance to the in-memory database
             ALL_ACCOUNTS[num_str] = new_acct
             print(f"loaded account '{num_str}'")
             return True
     except ValueError:
         print(f"error loading acct '{num_str}': balance value not a float")
-        return False
+    return False
     
 def load_all_accounts(acct_file = "accounts.txt"):
+    """ Load all accounts into the in-memory database, reading from a file in the same directory as the server application. """
     print(f"loading account data from file: {acct_file}")
     with open(acct_file, "r") as f:
         while True:
@@ -119,22 +130,71 @@ def load_all_accounts(acct_file = "accounts.txt"):
     print("finished loading account data")
     return True
 
-# Bank Server network operations
-# All code involved in supporting the bank server's network communications goes in this section
-def process_client_msg(msg_bytes):
-    # to be written
-    return
-
-def send_response_to_client(code, result):
-    # to be written
-    return
+##########################################################
+#                                                        #
+# Bank Server Network Operations                         #
+#                                                        #
+# TODO: THIS SECTION NEEDS TO BE WRITTEN!!               #
+#                                                        #
+##########################################################
 
 def run_network_server():
-# Needs to be written!
+    """ This and all supporting code needs to be written! """
+    print("Bank server network functions not implemented!!")
     return
+
+##########################################################
+#                                                        #
+# Bank Server Demonstration                              #
+#                                                        #
+# Demonstrate basic server functions.                    #
+# No changes needed in this section.                     #
+#                                                        #
+##########################################################
+
+def demo_bank_server():
+    """ A function that exercises basic server functions and prints out some results. """
+    # get the demo account from the database
+    acct = get_acct("zz-99999")
+    print(f"Test account '{acct.acct_number}' has PIN {acct.acct_pin}")
+    print(f"Current account balance: {acct.acct_balance}")
+    print(f"Attempting to deposit 123.45...")
+    _, code, new_balance = acct.deposit(123.45)
+    if not code:
+        print(f"Successful deposit, new balance: {new_balance}")
+    else:
+        print(f"Deposit failed!")
+    print(f"Attempting to withdraw 123.45 (same as last deposit)...")
+    _, code, new_balance = acct.withdraw(123.45)
+    if not code:
+        print(f"Successful withdrawal, new balance: {new_balance}")
+    else:
+        printf("Withdrawal failed!")
+    print(f"Attempting to deposit 123.4567...")
+    _, code, new_balance = acct.deposit(123.4567)
+    if not code:
+        print(f"Successful deposit (oops), new balance: {new_balance}")
+    else:
+        print(f"Deposit failed as expected, code {code}") 
+    print(f"Attempting to withdraw 12345.45 (too much!)...")
+    _, code, new_balance = acct.withdraw(12345.45)
+    if not code:
+        print(f"Successful withdrawal (oops), new balance: {new_balance}")
+    else:
+        print(f"Withdrawal failed as expected, code {code}")
+    print("End of demo!")
+
+##########################################################
+#                                                        #
+# Bank Server Startup Operations                         #
+#                                                        #
+# No changes needed in this section.                     #
+#                                                        #
+##########################################################
 
 if __name__ == "__main__":
     # on startup, load all the accounts from the account file
     load_all_accounts(ACCT_FILE)
+    demo_bank_server()
     run_network_server()
     print("bank server exiting...")
