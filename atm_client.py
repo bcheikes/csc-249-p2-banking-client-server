ae53eeaa7695dbf2d859e9be29920739b7bf9955
Brant Cheikes <brant@pedal-dancer.net>
2023-10-01 13:42:05 -0400

    Initial atm_client application added.

---
+++ atm_client.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+#
+# Automated Teller Machine (ATM) client application.
+
+import socket
+
+HOST = "127.0.0.1"      # The bank server's IP address
+PORT = 65432            # The port used by the bank server
+
+##########################################################
+#                                                        #
+# ATM Client Network Operations                          #
+#                                                        #
+# NEEDS REVIEW. Changes may be needed in this section.   #
+#                                                        #
+##########################################################
+
+def send_to_server(sock, msg):
+    """ Given an open socket connection (sock) and a string msg, send the string to the server. """
+    return sock.sendall(msg.encode('utf-8'))
+
+def get_from_server(sock):
+    """ Attempt to receive a message from the active connection. Block until message is received. """
+    msg = sock.recv(1024)
+    return msg.decode('utf-8')
+
+def login_to_server(sock, acct_num, pin):
+    """ Attempt to login to the bank server. Pass acct_num and pin, get response, parse and check whether login was successful. """
+    validated = 0
+    # TODO: Write this code! Note that a '1' return below is hardcoded until the network comms are built right.
+    return 1
+
+def get_login_info():
+    """ Get info from customer. TODO: Validate inputs, ask again if given invalid input. """
+    acct_num = input("Please enter your account number: ")
+    pin = input("Please enter your four digit PIN: ")
+    return acct_num, pin
+
+def process_deposit(sock, acct_num):
+    """ TODO: Write this code. """
+    amt = input("How much would you like to deposit? ")
+    return
+
+def get_acct_balance(sock, acct_num):
+    """ TODO: Ask the server for current account balance. """
+    bal = 0.0
+    # code needed here, to get balance from server then return it
+    return bal
+
+def process_withdrawal(sock, acct_num):
+    """ TODO: Write this code. """
+    bal = get_acct_balance(sock, acct_num)
+    amt = input(f"How much would you like to withdraw? (You have ${bal} available)")
+    return
+
+def process_customer_transactions(sock, acct_num):
+    """ Ask customer for a transaction, communicate with server. TODO: Revise as needed. """
+    while True:
+        print("Select a transaction. Enter 'd' to deposit, 'w' to withdraw, or 'x' to exit.")
+        req = input("Your choice? ").lower()
+        if req not in ('d', 'w', 'x'):
+            print("Unrecognized choice, please try again.")
+            continue
+        if req == 'x':
+            # if customer wants to exit, break out of the loop
+            break
+        elif req == 'd':
+            process_deposit(sock, acct_num)
+        else:
+            process_withdrawal(sock, acct_num)
+
+def run_atm_core_loop(sock):
+    """ Given an active network connection to the bank server, run the core business loop. """
+    acct_num, pin = get_login_info()
+    validated = login_to_server(sock, acct_num, pin)
+    if validated:
+        print("Thank you, your credentials have been validated.")
+    else:
+        print("Account number and PIN do not match. Terminating ATM session.")
+        return False
+    process_customer_transactions(sock, acct_num)
+    print("ATM session terminating.")
+    return True
+
+##########################################################
+#                                                        #
+# ATM Client Startup Operations                          #
+#                                                        #
+# No changes needed in this section.                     #
+#                                                        #
+##########################################################
+
+def run_network_client():
+    """ This function connects the client to the server and runs the main loop. """
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            run_atm_core_loop(s)
+    except Exception as e:
+        print(f"Unable to connect to the banking server - exiting...")
+
+if __name__ == "__main__":
+    print("Welcome to the ACME ATM Client, where customer delight is our goal!")
+    run_network_client()
+    print("Thanks for banking with us! Come again soon!!")
